15826018acd2eb39d50c8421cb133ca31dc4771b
phrolycheva <phrolycheva@gmail.com>
2020-04-20 14:48:45 +0300

    Добавление атомов

--- ChemistryTool/structures/molecule.py
+++ ChemistryTool/structures/molecule.py
@@ -3,7 +3,14 @@ from ..algorithms import Isomorphism
 
 
 class Molecule(Isomorphism, MoleculeABC):
-    ...
+    def add_atom(self, element: Element, number: int):
+        if isinstance(element, str) and isinstance(number, int):
+            self._atoms[number] = element
+            self._bonds[number] = {}
+
+    def add_bond(self, start_atom: int, end_atom: int, bond_type: int):
+        ...
+
 
 
 __all__ = ['Molecule']
